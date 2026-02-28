2ef33d80fb96b6895ac6d312bbcf351ad4c3ccdb
olayinka.ibrahim <olayinka.ibrahim@etranzact.com>
2026-02-28 17:57:54 +0100

    updated code

--- src/validators/@base-schema.ts
+++ src/validators/@base-schema.ts
@@ -4,7 +4,6 @@ import { passwordRegExp } from "./regex";
 export const BaseSchema = z.object({
     email: z.email(),
     name: z.string().min(1, "Name is required"),
-    token: z.string().min(6, "Token is required"),
     username: z.string().min(1, { message: "Username is required" }),
     firstName: z.string().min(1, "First name is required"),
     lastName: z.string().min(1, "Last name is required"),
